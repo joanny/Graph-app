6014fcb3143575c7daac70c7a8131833106ada58
joanny <joannysimpore@gmail.com>
2013-11-18 12:09:12 +0100

    ajout search.html

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import play.*;
+import play.data.validation.Required;
 import play.db.jpa.GenericModel.JPAQuery;
 import play.mvc.*;
 
@@ -10,13 +11,26 @@ import models.*;
 
 public class Application extends Controller {
 
+	   
+			
 	 public static void index(){
-		 Graph graph = new Graph();
-    	 List<Relations> allNodes =  Relations.findAll();
-    	 for (Relations relations : allNodes) {
-			 graph.addRoute(relations.start_node, relations.end_node, relations.weight);
-    	 }
-    	 List<String> val = graph.findRoute("Paris", "Biarritz");
-    	 render(val);
+		 
+		   Graph graph = new Graph();  
+		 List<Nodes> lesNodes = Nodes.findAll();	
+		  render(lesNodes);
     }
+	 
+	 public static void search(@Required String start,@Required String end){
+		 
+		  	Graph graph = new Graph();  
+			List<Nodes> lesNodes = Nodes.findAll();	
+			 
+	    	List<Relations> allNodes =  Relations.findAll();
+	    	for (Relations relations : allNodes) {
+				 graph.addRoute(relations.start_node, relations.end_node, relations.weight);
+	    	}    	 
+	    	List<String> val = graph.findRoute(start, end);
+	    	render(lesNodes,val); 
+		 
+	 }
 }
